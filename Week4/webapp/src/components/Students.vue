7c1ea3d768567028c4877c9e57061938e79c5cd2
ElectricTaco27 <46634949+ElectricTaco27@users.noreply.github.com>
2019-02-16 18:09:21 -0800

    Add files via upload
    (had to recreate repos)
    adding back week 4

---
+++ Week4/webapp/src/components/Students.vue
@@ -0,0 +1,65 @@
+<template>
+    <div>
+        <h1 class ="testClass">Student Table</h1>
+        <table>
+            <thead>
+                <tr>
+                    <th>Student ID</th>
+                    <th>Email Address</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="student in students" :key="student.id">
+                    <td> {{ student.id }} </td>
+                    <td> {{ student.email }} </td>
+                </tr>
+            </tbody>
+        </table>
+    </div> 
+</template>
+
+<script>
+    export default { 
+        name: 'Students',
+        data () {
+            return {
+                students: 
+                [
+                    {"id": 0,"email": "SAO@Gmail.com"},
+                    {"id": 1,"email": "DanMachi@Hotmail.com"},
+                    {"id": 2,"email": "FatalBullet@Yahoo.com"},
+                    {"id": 3,"email": "Apex@Aol.com"},
+                    {"id": 4,"email": "Crunchyroll@Gmail.com"}
+                ]
+            }
+        },
+        methods: 
+        {
+            getStudents: function() 
+            {
+                return JSON.parse(testStudents).students; 
+            }
+        },
+        mounted() {
+            this.getStudents();
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+    /* <link href="sharedcssfile.css" rel="stylesheet" type="text/css"> */
+    table {
+        font-family: arial;
+        border-collapse: collapse;
+        width: 100%;
+    }
+    td, th {
+        border: 1px solid #dddddd;
+        text-align: left;
+        padding: 8px;
+    }
+    tr:nth-child(even){
+        background-color: #dddddd;
+    }
+</style>
